27f1450364d1f8f69606db98fccf97d7b2953cb8
Corey Frenette <LordMonoxide@users.noreply.github.com>
2015-06-16 22:06:43 -0300

    Update Value.php

--- src/Values/Value.php
+++ src/Values/Value.php
@@ -3,11 +3,14 @@
 abstract class Value {
   private $value = null;
   
-  protected function __construct($value) {
+  public function __construct($value) {
+    $this->validate($value);
     $this->value = $value;
   }
   
   public function value() {
     return $this->value;
   }
+  
+  protected abstract function validate($value);
 }
